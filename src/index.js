d385b523ce39130f5c442263aff721fdd43cc65c
DariaMakk <68727372+DariaMakk@users.noreply.github.com>
2020-10-20 20:44:18 +0300

    Update index.js
    "feat: add solution"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsList = bracketsConfig.map((item) => item.join(""));
+  let i = 0;
+  while (i < bracketsList.length) {
+    if (str.includes(bracketsList[i])) {
+      while(str.includes(bracketsList[i])) {
+        str = str.replace(bracketsList[i], '');
+      }
+      i = 0;
+    } else {
+      i++;
+    }    
+  }
+  return str.length === 0 ? true : false;
 }
